8170b5bb594cc9ffe25f703c4c1f92809468b9fd
shrea1456 <shreashidutta1406@gmail.com>
2026-01-20 13:08:56 +0530

    Add files via upload

---
+++ node.py
@@ -0,0 +1,15 @@
+class SHREASHI:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+        self.parent=None
+root=None
+a = [1,10,11,8,6,12,14,7]
+print(root)
+root = SHREASHI(a[0])
+print(root)
+print(root.data)
+b = SHREASHI(a[5])
+print(b)
+print(b.data)
